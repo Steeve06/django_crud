914a7dd9615b4be47f81e420de282015d18ba442
Steeve Mocto <moctosteeve@gmail.com>
2025-03-12 15:08:05 -0400

    initializing forms for registering users

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # home page
     # path('login/', views.login_user, name='login'), uncommented this section if you want to create a separate login page
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
